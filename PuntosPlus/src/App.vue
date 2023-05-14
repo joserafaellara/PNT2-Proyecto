6ca0450a6031ede6a3acc6d7d43d322e36475cf1
Lara Jose <jose.rafa.189@gmail.com>
2023-05-14 20:33:31 -0300

    inicio Proyecto

---
+++ PuntosPlus/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+  import Cabecera from './components/Cabecera.vue'
+  import Pie from './components/Pie.vue'
+  import Proyecto from './components/Proyecto.vue'
+ 
+
+  export default
+  (await import('vue')).defineComponent({
+    components: {Cabecera,Pie,Proyecto},
+    data(){
+      return{
+        nEmpresa : "Grupo: Puntos Plus"
+      }
+    }
+  })
+  
+</script>
+
+<template>
+   <div>
+      <Cabecera/>
+      <Proyecto/>
+      <Pie :empresa="nEmpresa" />
+   </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ PuntosPlus/src/components/Pie.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>   
+        <p> Realizado por {{ empresa }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['empresa']
+}
+</script>
+
+<style>
+
+</style>
